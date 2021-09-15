c9b393a4ff4086c8c7c5e9f8501fef35de9aa41f
Jongold00 <jongold00@gmail.com>
2021-09-14 22:39:48 -0400

    figured out how to connect to server

--- Project 1/client.py
+++ Project 1/client.py
@@ -1,1 +1,60 @@
-PORT = 5050
+
+import socket
+
+def main():
+    HOST = 'proj1.3700.network'  # The server's hostname or IP address
+    PORT = 27993
+    NUID = '001450798'
+
+
+    HELLO_msg = "ex_string HELLO " + NUID + "\n"
+
+
+    # framework code courtesy of https://realpython.com/python-sockets/
+
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.send(HELLO_msg.encode())
+        data = s.recv(8192)
+        print(data.decode())
+        #conn, addr = s.accept()
+        """ with conn:
+            while True:
+                #data = conn.recv(8192)
+                if not data:
+                    break
+
+
+                msg_type = data.split()[0]
+
+                SendMessage(msg_type)
+                if (msg_type == 'FIND'):
+                    token_count = GetTokenOccurance(data.split()[1], data.split()[2])
+                    msg = 'COUNT ' + token_count + '\n'
+                    s.send(msg)
+                if (msg_type == 'BYE'):
+                    print(data.split()[1])
+
+                conn.sendall(data)
+ """
+    #print('Received', repr(data))
+
+def SendMessage(msg_type):
+    return
+    
+
+def GetTokenOccurance(token, message):
+    return  message.count(token)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
